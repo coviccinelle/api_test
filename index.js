9b710c897eddc6d4142d0d3d807aa6aeac9be341
coviccinelle <coviccinelle@gmail.com>
2026-05-28 16:33:24 +0200

    reconnection retry

--- index.js
+++ index.js
@@ -2,41 +2,60 @@ const Websocket = require('ws');
 
 const wsUrl = 'wss://stream.binance.com:9443/ws/bnbbtc@depth';
 
-// start connection
-const ws = new Websocket(wsUrl);
+let ws;
+let reconnectTimeout = null;
 
-// event when connection starblished
+function connect()
+{
+	console.log('Connecting to Binance Websocket!');
+	// 1. start connection
+	ws = new Websocket(wsUrl);
 
-ws.on('open', () => {
-	console.log('Connection etablished to Binance Websocket!');
-});
+	// 2. event when connection etablished
 
-// event when received data
-ws.on('message', (data) => {
-	try {
-		const eventData = JSON.parse(data);
+	ws.on('open', () => {
+		console.log('Connection etablished to Binance Websocket!');
+		if (reconnectTimeout){
+			clearTimeout(reconnectTimeout);
+			reconnectTimeout = null;
+		}
+	});
 
-		const time = new Date(eventData.E).toLocaleTimeString();
+	// 3. event when received data
+	ws.on('message', (data) => {
+		try {
+			const eventData = JSON.parse(data);
+			const time = new Date(eventData.E).toLocaleTimeString();
 
-		
-		let highestBid = 0; 
-		let highestAsk = 0; 
-		highestBid = eventData.b[0] ? eventData.b[0][0] : 'N/A'; //highest buying price
-		lowestAsk = eventData.a[0] ? eventData.a[0][0] : 'N/A'; //lowest seliing price
+			let highestBid = eventData.b[0] ? eventData.b[0][0] : 'undefined'; //highest buying price
+			let lowestAsk = eventData.a[0] ? eventData.a[0][0] : 'undefined'; //lowest seliing price
 
-		console.log(`[${time}] BNB/BTC | Best Bid: ${highestBid} | Best Ask: ${lowestAsk}`);
+			console.log(`[${time}] BNB/BTC | Best Bid: ${highestBid} | Best Ask: ${lowestAsk}`);
 
-	} catch (error) {
-		console.error('Error parsing JSON:', error);
-	}
-});
+		} catch (error) {
+			console.error('Error parsing JSON:', error);
+		}
+	});
 
-// process when error
-ws.on('error', (error) => {
-	console.error('Error connection: ', error);
-});
+	// 4. process when error
+	ws.on('error', (error) => {
+		console.error('Error connection: ', error);
+	});
 
-// process error when having a closed connection
-ws.on('close', () => {
-	console.log('Socket connection is closed, try try to reconnect?');
-});
+	// 5. process error when having a closed connection
+	ws.on('close', () => {
+		console.log('Socket connection is closed');
+
+		if (!reconnectTimeout)
+		{
+			console.log('trying to reconnect in 5 secondes...');
+			reconnectTimeout = setTimeout(() => {
+
+				reconnectTimeout = null;
+				connect();
+			}, 5000);
+		}
+	});
+}
+
+connect();
