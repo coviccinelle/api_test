9e0df6158923949a113eb3b53b2c3a5895bae001
coviccinelle <coviccinelle@gmail.com>
2026-05-28 16:35:32 +0200

    this is what I couldn't do during a tech test 27/05/2026. such a shame, but it's never too late to learn, it's life. Don't forget this lesson pleaseeeee

--- index.js
+++ index.js
@@ -2,27 +2,24 @@ const Websocket = require('ws');
 
 const wsUrl = 'wss://stream.binance.com:9443/ws/bnbbtc@depth';
 
-// start connection
+// 1. start connection
 const ws = new Websocket(wsUrl);
 
-// event when connection starblished
+// 2. event when connection starblished
 
 ws.on('open', () => {
 	console.log('Connection etablished to Binance Websocket!');
 });
 
-// event when received data
+// 3. event when received data
 ws.on('message', (data) => {
 	try {
 		const eventData = JSON.parse(data);
 
 		const time = new Date(eventData.E).toLocaleTimeString();
 
-		
-		let highestBid = 0; 
-		let highestAsk = 0; 
-		highestBid = eventData.b[0] ? eventData.b[0][0] : 'N/A'; //highest buying price
-		lowestAsk = eventData.a[0] ? eventData.a[0][0] : 'N/A'; //lowest seliing price
+		let highestBid = eventData.b[0] ? eventData.b[0][0] : 'N/A'; //highest buying price
+		let lowestAsk = eventData.a[0] ? eventData.a[0][0] : 'N/A'; //lowest seliing price
 
 		console.log(`[${time}] BNB/BTC | Best Bid: ${highestBid} | Best Ask: ${lowestAsk}`);
 
@@ -31,12 +28,12 @@ ws.on('message', (data) => {
 	}
 });
 
-// process when error
+// 4. process when error
 ws.on('error', (error) => {
 	console.error('Error connection: ', error);
 });
 
-// process error when having a closed connection
+// 5. process error when having a closed connection
 ws.on('close', () => {
-	console.log('Socket connection is closed, try try to reconnect?');
+	console.log('Socket connection is closed, try to reconnect?');
 });
