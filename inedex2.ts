e23d8dd77301af2e0b69473dd8d53163d3a27fa8
coviccinelle <coviccinelle@gmail.com>
2026-05-31 14:23:03 +0200

    add comments for index2

--- inedex2.ts
+++ inedex2.ts
@@ -2,12 +2,15 @@ const Websocket = require('ws');
 
 const wsUrl = 'wss://stream.binance.com:9443/ws/bnbbtc@depth';
 
+// 1. create new socket
 const ws = new Websocket(wsUrl);
 
+// 2. open socket
 ws.on('open', () => {
 	console.log("socket opened");
 });
 
+// 3. start listening
 ws.on('message', (data) => {
 	try{
 		console.log("receiving data");
@@ -19,10 +22,12 @@ ws.on('message', (data) => {
 	}
 });
 
+// 4.error socket
 ws.on('error', () => {
 	console.log("Websocket error: ", error);
 });
 
+// 5. close socket
 ws.on('close', () => {
 	console.log("Websocket closed");
 });
